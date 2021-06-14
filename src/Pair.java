9c9ee4940ffb67db228ee2cb84e17a5c894f715a
shakti singh <shakti.singh@copart.com>
2021-06-14 11:40:05 -0500

    First commit - Java Generics

---
+++ src/Pair.java
@@ -0,0 +1,13 @@
+public class Pair<X,Y> {
+    private X first;
+    private Y second;
+
+    public Pair(X a1, Y a2){
+        first = a1;
+        second = a2;
+    }
+
+    public X getFirst(){return first;}
+    public Y getSecond(){return second;}
+
+}
